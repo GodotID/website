4dcf0505769d7a56018ebf419095527dc23382b7
HanzCEO <hanzhaxors@gmail.com>
2022-04-30 15:28:55 +0700

    feat(NavBar): Added NavBar content

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,27 @@
 // Modules
 import React from 'react'
-import Link from 'next/link'
+
+// Components
+import Link from './Primitives/Link'
+import Flex from './Primitives/Flex'
+
+// Styles
+import style from './NavBar.module.scss'
 
 export default function NavBar() {
 	return (
-		<div></div>
+		<Flex className={style.NavBar} spacebetween="true" verticalcenter="true" width={-2}>
+			<h3>GodotID</h3>
+			<Flex gap={4}>
+				<Link href="/">Home</Link>
+				<Link href="/gallery">Gallery</Link>
+				<Link href="/jams">Game Jams</Link>
+				<Link href="/budget">Budget</Link>
+				<Link href="/team">Contributors</Link>
+			</Flex>
+			<Link href="https://t.me/godot_indonesia">
+				Join Us
+			</Link>
+		</Flex>
 	);
 }
