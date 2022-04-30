bc0affe87a7c5a232c653ba3d6e6688a50e22d5f
HanzCEO <hanzhaxors@gmail.com>
2022-04-30 15:34:59 +0700

    feat(Link): Added Link component

---
+++ components/Primitives/Link.tsx
@@ -0,0 +1,18 @@
+// Modules
+import React from 'react'
+
+// Components
+import NextLink from 'next/link'
+
+// Styles
+import style from './Link.module.scss'
+
+export default function Link(props: any) {
+	return (
+		<NextLink href={props.href}>
+			<a className={style.a}>
+				{props.children}
+			</a>
+		</NextLink>
+	);
+};
