ce76cce9f7541a1871c7a77928c7034110ea2b44
HanzCEO <hanzhaxors@gmail.com>
2022-04-30 15:29:47 +0700

    feat(Hero): Added Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+// Modules
+import React from 'react'
+
+// Components
+import Flex from './Primitives/Flex'
+import Link from './Primitives/Link'
+
+// Styles
+import style from './Hero.module.scss'
+
+export default function(props) {
+	return (
+		<Flex className={style.Hero}>
+			<Flex direction={1} width={1}>
+				<h1>
+					Komunitas Game Dev Godot <b>#1 di Indonesia</b>
+				</h1>
+				<p id="subtitle">
+					Gabung bersama lebih dari 200 godotters Indonesia di komunitas GodotID. Ikuti banyak event menarik seperti Game Jam dan webinar yang diselenggarakan oleh komunitas!
+				</p>
+				<Link href="https://t.me/godot_indonesia">Join Us</Link>
+			</Flex>
+			<Flex width={1}></Flex>
+			<style jsx>{`
+				h1 { width: 18ch }
+				#subtitle { width: 90% }
+			`}</style>
+		</Flex>
+	);
+}
