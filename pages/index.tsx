35fa838e7c9f318a328d21fa9f1266c471a20290
HanzCEO <hanzhaxors@gmail.com>
2022-05-11 15:52:32 +0700

    feat(index): Added "Visi Misi" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,12 +38,42 @@ function EducationFocusedSection() {
 	)
 }
 
+function VisiMisiSection() {
+	// TODO: [45] Change this inline style into something
+	// TODO: [46] Change <span> to something that could color text
+	return (
+		<Flex direction={1} style={{marginTop: '5em'}}>
+			<span><small><b>Education focused community</b></small></span>
+			<Flex gap="10">
+				<div>
+					<h3>
+						Menggunakan game development<br/>
+						sebagai <b>entry point</b> untuk<br/>
+						masuk dunia programming.<br/>
+					</h3>
+					<h1>Visi</h1>
+				</div>
+				<div>
+					<ul>
+						<li>Mensosialisasikan komunitas ke orang tua atau wali</li>
+						<li>Membuat webinar dan event-event menarik</li>
+						<li>Membuka kesempatan untuk berkontribusi bagi anggota</li>
+					</ul>
+					<h1>Misi</h1>
+				</div>
+			</Flex>
+		</Flex>
+	)
+}
+
 export default function IndexPage() {
 	return (
 		<Page title="GodotID">
 			<Hero />
 			{/* Education Focused Community */}
 			<EducationFocusedSection />
+			{/* Visi Misi */}
+			<VisiMisiSection />
 		</Page>
 	)
 }
