fd0a27ced7d922fd23e0e84e0888691d08218ee1
bluespada <pentingmain@gmail.com>
2022-07-07 13:55:24 +0800

    fix: hydration warning , and improvment on redirection.
    - redirection bugs if parameters to is undefined or empty
    - fix hydration error

--- components/Page.tsx
+++ components/Page.tsx
@@ -8,20 +8,20 @@ import Footer from './Footer'
 
 interface PageProps {
 	title?: string;
-	children?: any;
+	children?: React.ReactNode;
 }
 
 export default function Page({ title = "", children }: PageProps) {
 	return (
-	<React.Fragment>
+	<>
 		<Head>
 			<title>{title}</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" key="responsive" />
 			<meta property="og:title" content={title} key="title" />
 		</Head>
-		<NavBar />
+		<NavBar/>
 		{children}
 		<Footer />
-	</React.Fragment>
+	</>
 	);
 }
