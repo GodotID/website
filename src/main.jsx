edd7b9bce8c3a2bbcbc47950c2440aaeb85b3428
Hanz <hanz@godot.id>
2023-06-28 18:59:13 +0700

    feat(main): Added css files

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import { Routes } from '@generouted/react-router'
 
+import 'misc/fonts/Lexend/stylesheet.css'
+import 'misc/fonts/Philosopher/stylesheet.css'
+import 'styles/general.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
