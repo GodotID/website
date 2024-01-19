751b0b7cec8879a1c65af90c67471ecb6cfe8a8d
Hanz <hanz@godot.id>
2024-01-19 15:58:00 +0700

    fix: Fixed routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, useRoutes } from 'react-router-dom';
 
-import { Routes } from '@generouted/react-router'
+import { routes } from '@generouted/react-router'
 
 import 'misc/fonts/Lexend/stylesheet.css'
 import 'misc/fonts/Philosopher/stylesheet.css'
 import 'styles/general.css'
 
+function Router() {
+    return useRoutes(routes);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Routes />
-	</React.StrictMode>
+    <HashRouter>
+        <Router />
+    </HashRouter>
 );
