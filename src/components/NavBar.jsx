46a5c2eef3c4a64b2c719e183eba7fa76560f12b
Hanz <hanz@godot.id>
2024-01-19 16:21:32 +0700

    feat(Homepage): Added navbar for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 import 'styles/components/navbar.css'
 
 import { ReactComponent as GodotIDLogo } from 'misc/imgs/logo.svg'
+import { Menu, X } from 'lucide-react';
 
 const NavBar = ({ color }) => {
     if (!color) color = 'primary';
+
+    let [isClosed, setClose] = useState('navbar__closed');
 
 	return (
 		<div className={["navbar__navbar", color].join(' ')}>
@@ -15,7 +18,13 @@ const NavBar = ({ color }) => {
 			</div>
 			<div className="navbar__spaceWrapper">
 				<h3>Godot Indonesia</h3>
-				<div className="navbar__linksWrapper">
+                <div className="navbar__mobile">
+                    <Menu onClick={() => setClose('')}/>
+                </div>
+				<div className={"navbar__linksWrapper " + isClosed}>
+                    <div className='navbar__mobile navbar__mobile__close'>
+                        <X onClick={() => setClose('navbar__closed')} />
+                    </div>
 					<a href="#profil">Profil</a>
 					<a href="https://itch.io/jams/hosted-by-hanzceo">Kompetisi</a>
 					<Link to="/gallery">Galeri</Link>
